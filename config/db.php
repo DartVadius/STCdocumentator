672631e21c9f9f44074c1f825cf8b8959a9d3750
Vadym <vad261275@gmail.com>
2017-07-19 10:50:39 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=documentator',
-    'username' => 'admin777',
-    'password' => 'qwerty777',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
 ];
